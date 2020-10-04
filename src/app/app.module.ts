c4806e81fb1089474942e2267128b4d7d5e7a7c5
marianesfg <mariane.sfgodoy@gmail.com>
2020-10-04 18:42:58 -0300

    'ts'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,46 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule}  from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { EmployeeComponent } from './employee/employee.component';
+import { ShowEmpComponent } from './employee/show-emp/show-emp.component';
+import { EditEmpComponent } from './employee/edit-emp/edit-emp.component';
+import { AddEmpComponent } from './employee/add-emp/add-emp.component';
+import { DepartmentComponent } from './department/department.component';
+import { AddDepComponent } from './department/add-dep/add-dep.component';
+import { ShowDepComponent } from './department/show-dep/show-dep.component';
+import { EditDepComponent } from './department/edit-dep/edit-dep.component';
+
+import { DepartmentService } from 'src/app/services/department.service';
+import { EmployeeService } from 'src/app/services/employee.service';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    ShowEmpComponent,
+    EditEmpComponent,
+    AddEmpComponent,
+    DepartmentComponent,
+    AddDepComponent,
+    ShowDepComponent,
+    EditDepComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    //MatTableDataSource
+    MatTableModule, 
+    MatIconModule, 
+    MatButtonModule
   ],
-  providers: [],
+  providers: [DepartmentService,EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
